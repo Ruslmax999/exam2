8722e6225ed535e73683f2b02757e01cfb3422b1
Ruslan Muginov <ruslmax99@gmail.com>
2021-12-04 17:03:02 +0200

    fix

--- build/js/script.js
+++ build/js/script.js
@@ -15488,46 +15488,46 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   lightbox2__WEBPACK_IMPORTED_MODULE_15___default.a.option({
-    'resizeDuration': 200,
-    'wrapAround': true
+    resizeDuration: 200,
+    wrapAround: true
   }); // Sticky header
 
-  var header = document.querySelector('.header-wrap');
+  var header = document.querySelector(".header-wrap");
   var options = {
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 1
   };
 
   var callback = function callback(items) {
     items.forEach(function (item) {
-      return !item.isIntersecting ? header.classList.add('sticky') : header.classList.remove('sticky');
+      return !item.isIntersecting ? header.classList.add("sticky") : header.classList.remove("sticky");
     });
   };
 
   var observer = new IntersectionObserver(callback, options);
-  var target = document.querySelector('.swiper-pagination');
+  var target = document.querySelector(".swiper-pagination");
   observer.observe(target); /// menu change title
 
   var options2 = {
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 0.1
   };
 
-  var targets = _toConsumableArray(document.querySelectorAll('section'));
+  var targets = _toConsumableArray(document.querySelectorAll("section"));
 
-  var menuItems = _toConsumableArray(document.querySelectorAll('.header__nav a'));
+  var menuItems = _toConsumableArray(document.querySelectorAll(".header__nav a"));
 
   var observer1 = new IntersectionObserver(function (items) {
     items.forEach(function (item) {
       if (item.isIntersecting) {
-        var sectionId = item.target.getAttribute('id');
+        var sectionId = item.target.getAttribute("id");
         menuItems.forEach(function (navItem) {
-          navItem.classList.remove('link-active');
+          navItem.classList.remove("link-active");
 
-          if (navItem.getAttribute('data-link') === sectionId) {
-            navItem.classList.add('link-active');
+          if (navItem.getAttribute("data-link") === sectionId) {
+            navItem.classList.add("link-active");
           }
         });
       }
@@ -15537,35 +15537,25 @@ document.addEventListener('DOMContentLoaded', function () {
     observer1.observe(targets[index]);
   }); // Smooth scroll
 
-  $('a[href*="#"]') // Remove links that don't actually link to anything
-  .not('[href="#"]').not('[href="#0"]').click(function (event) {
-    // On-page links
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      // Figure out element to scroll to
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']'); // Does a scroll target exist?
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
 
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-        $('html, body').animate({
+        $("html, body").animate({
           scrollTop: target.offset().top
         }, 1000, function () {
-          // Callback after animation
-          // Must change focus!
           var $target = $(target);
           $target.focus();
 
           if ($target.is(":focus")) {
-            // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-
-            $target.focus(); // Set focus again
+            $target.attr("tabindex", "-1");
+            $target.focus();
           }
-
-          ;
         });
       }
     }
@@ -15618,13 +15608,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }); // mobile menu
 
-  var nav = $('#mobile-menu'),
-      overlay = $('.overlay'),
-      menuToggle = $('.menu-button, .overlay');
-  menuToggle.on('click', function (e) {
+  var nav = $("#mobile-menu"),
+      overlay = $(".overlay"),
+      menuToggle = $(".menu-button, .overlay");
+  menuToggle.on("click", function (e) {
     e.preventDefault();
-    nav.toggleClass('open');
-    overlay.toggleClass('overlay-open');
+    nav.toggleClass("open");
+    overlay.toggleClass("overlay-open");
   });
 });
 
@@ -15637,8 +15627,8 @@ document.addEventListener('DOMContentLoaded', function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\user\WebstormProjects\Exam2\src\js\script.js */"./src/js/script.js");
-module.exports = __webpack_require__(/*! C:\Users\user\WebstormProjects\Exam2\src\js\map\map.js */"./src/js/map/map.js");
+__webpack_require__(/*! C:\Users\user\Desktop\Exam2\src\js\script.js */"./src/js/script.js");
+module.exports = __webpack_require__(/*! C:\Users\user\Desktop\Exam2\src\js\map\map.js */"./src/js/map/map.js");
 
 
 /***/ })
